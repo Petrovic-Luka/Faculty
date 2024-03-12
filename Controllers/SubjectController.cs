9a25f43eb4ca4c0939b27eb9a2c918300653d323
Petrovic-Luka <lukapetrovic00@gmail.com>
2024-03-12 19:58:50 +0100

    Add project files.

---
+++ Controllers/SubjectController.cs
@@ -0,0 +1,86 @@
+﻿using Faculty.Common.Models.Subject;
+using Faculty.Core;
+using Faculty.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Faculty.Api.Controllers;
+
+[ApiController]
+[Route("api/subjects")]
+public class SubjectController : ControllerBase
+{
+    private readonly ISubjectManager subjectManager;
+
+    public SubjectController(ISubjectManager subjectManager)
+    {
+        this.subjectManager = subjectManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllSubjects()
+    {
+        try
+        {
+            return Ok(await subjectManager.GetAllSubjects());
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500);
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult>CreateSubject(SubjectCreateModel model)
+    {
+        try
+        {
+            await subjectManager.Create(model);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+       
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteSubject(Guid guid)
+    {
+        try
+        {
+            await subjectManager.Delete(guid);
+            return Ok();
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500);
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateSubject(Subject subject)
+    {
+        try
+        {
+            await subjectManager.Update(subject);
+            return Ok();
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception)
+        {
+            return StatusCode(500);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Faculty;
+using Faculty.Core;
+using Faculty.Data.InMemory;
+using Faculty.Data.Sql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSingleton<ISubjectManager, SubjectManager>();
+builder.Services.AddSingleton<ISubjectRepository, SubjectSqlRepository>();
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
